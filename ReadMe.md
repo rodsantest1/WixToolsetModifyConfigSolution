05022c817b0f84587dd7c0ec3bdf34587d65f923
Rodney Santiago <rodchar.owner@outlook.com>
2020-09-29 20:45:03 -0400

    Notable - read appsettings file in web api program.cs file

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,8 @@
 # Wix Toolset - Modifying AppSettings file during an install  
 
 Article [[Go]](http://blog.wibeck.org/2014/03/howto-to-set-appsettings-during-installation-using-wix/)  
+
+
+Found this SO for reading config values in appsettings.json file [[Go]](https://stackoverflow.com/a/41738816/139698)   
+
+
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -16,11 +17,22 @@ namespace WebApplication1
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseUrls(config["BaseUrl"])
+                        .UseConfiguration(config)
+                        .UseStartup<Startup>();
                 });
+        }
     }
 }
